332077621515c241beaec88c2fc49b55a1a74166
Darrin Holst <darrinholst@gmail.com>
2009-01-30 20:41:42 -0600

    version 1.2

--- content/fitnesse-formatter.js
+++ content/fitnesse-formatter.js
@@ -5,108 +5,270 @@
   var load_css = function() {
     var css = doc.createElement('style');
     css.type = 'text/css';
-    css.innerHTML = 'textarea { overflow: scroll; } '
+    css.innerHTML = 
+      'textarea.no_wrap { overflow: scroll; } ' +
+      'div.edit_buttons { float: left; }' +
+      'div.edit_options { float: right; }' +
+      'div.hints { clear: both; }'
     doc.getElementsByTagName('head')[0].appendChild(css);    
   }
-  
+
+  var format_content = function(wikiText) {
+    var formatted = "";
+    var currentTable = [];
+    var lines = wikiText.split("\n");
+    var line = null;
+
+    for(var i = 0, j = lines.length; i < j; i++) {
+      line = lines[i];
+
+      if(is_table_row(line)) {
+        currentTable.push(line);
+      }
+      else {
+        formatted += format_table(currentTable);
+        currentTable = [];        
+        formatted += line + "\n";
+      }
+    }
+
+    formatted += format_table(currentTable);
+    return formatted.slice(0, formatted.length - 1);
+  }
+
+  var format_table = function(table) {
+    var formatted = "";
+    var rows = split_rows(table);
+    var widths = calculate_column_widths(rows);
+    var row = null;
+
+    for(var rowIndex = 0, numberOfRows = rows.length; rowIndex < numberOfRows; rowIndex++) {
+      row = rows[rowIndex];
+      formatted += "|";
+
+      for(var columnIndex = 0, numberOfColumns = row.length; columnIndex < numberOfColumns; columnIndex++) {
+        formatted += right_pad(row[columnIndex], widths[rowIndex][columnIndex]) + "|";
+      }
+
+      formatted += "\n";
+    }
+
+    return formatted;
+  }
+
+  var calculate_column_widths = function(rows) {
+    var widths = get_real_column_widths(rows);
+    var totalNumberOfColumns = get_number_of_columns(rows);
+
+    var maxWidths = get_max_widths(widths, totalNumberOfColumns);    
+    set_max_widths_on_non_colspan_columns(widths, maxWidths);
+
+    var colspanWidths = get_colspan_width(widths, totalNumberOfColumns);
+    adjust_widths_for_colspans(widths, maxWidths, colspanWidths);
+
+    adjust_colspans_for_widths(widths, maxWidths);
+
+    return widths;
+  }
+
   var is_table_row = function(line) {
     return line.indexOf('|') == 0;
   }
-  
-  var split_row = function(line) {
-    var parts = $.trim(line).split('|');
-    parts = parts.slice(1, parts.length - 1);
-    
-    for(var i = 0, j = parts.length; i < j; i++) {
-      parts[i] = $.trim(parts[i]);
+
+  split_rows = function(rows) {
+    var split_rows = [];
+
+    each(rows, function(row) {
+      split_rows.push(split_row(row));
+    }, this);
+
+    return split_rows;
+  }
+
+  var split_row = function(row) {
+    var columns = trim(row).split('|');
+    columns = columns.slice(1, columns.length - 1);
+
+    each(columns, function(column, i) {
+      columns[i] = trim(column);
+    }, this);
+
+    return columns;
+  }
+
+  var get_real_column_widths = function(rows) {
+    var widths = [];
+
+    each(rows, function(row, rowIndex) {
+      widths.push([]);
+
+      each(row, function(column, columnIndex) {
+        widths[rowIndex][columnIndex] = column.length;
+      }, this);
+    }, this);
+
+    return widths;
+  }
+
+  var get_max_widths = function(widths, totalNumberOfColumns) {
+    var maxWidths = [];
+    var row = null;
+
+    each(widths, function(row, rowIndex) {
+      each(row, function(columnWidth, columnIndex) {
+        if(columnIndex == (row.length - 1) && row.length < totalNumberOfColumns) {
+          return false;
+        }
+
+        if(columnIndex >= maxWidths.length) {
+          maxWidths.push(columnWidth);
+        }
+        else if(columnWidth > maxWidths[columnIndex]) {
+          maxWidths[columnIndex] = columnWidth;
+        }        
+      }, this);
+    }, this);
+
+    return maxWidths;
+  }
+
+  var get_number_of_columns = function(rows) {
+    var numberOfColumns = 0;
+
+    each(rows, function(row) {
+      if(row.length > numberOfColumns) {
+        numberOfColumns = row.length;
+      }
+    });
+
+    return numberOfColumns;
+  }
+
+  var get_colspan_width = function(widths, totalNumberOfColumns) {
+    var colspanWidths = [];
+    var colspan = null;
+    var colspanWidth = null;
+
+    each(widths, function(row, rowIndex) {
+      if(row.length < totalNumberOfColumns) {
+        colspan = totalNumberOfColumns - row.length;
+        colspanWidth = row[row.length - 1];
+
+        if(colspan >= colspanWidths.length) {
+          colspanWidths[colspan] = colspanWidth;
+        }
+        else if(!colspanWidths[colspan] || colspanWidth > colspanWidths[colspan]) {
+          colspanWidths[colspan] = colspanWidth;
+        }
+      }
+    });
+
+    return colspanWidths;
+  }
+
+  var set_max_widths_on_non_colspan_columns = function(widths, maxWidths) {
+    each(widths, function(row, rowIndex) {
+      each(row, function(columnWidth, columnIndex) {
+        if(columnIndex == (row.length - 1) && row.length < maxWidths.length) {
+          return false;
+        }
+
+        row[columnIndex] = maxWidths[columnIndex];
+      }, this);
+    }, this);
+  }
+
+  var get_width_of_last_number_of_columns = function(maxWidths, numberOfColumns) {
+    var width = 0;
+
+    for(var i = 1; i <= numberOfColumns; i++) {
+      width += maxWidths[maxWidths.length - i]
     }
 
-    return parts;
+    return width + numberOfColumns - 1; //add in length of separators
   }
-  
+
+  var spread_out_excess_over_last_number_of_columns = function(maxWidths, excess, numberOfColumns){
+    var columnToApplyExcessTo = maxWidths.length - numberOfColumns;
+
+    for(var i = 0; i < excess; i++) {
+      maxWidths[columnToApplyExcessTo++] += 1;
+
+      if(columnToApplyExcessTo == maxWidths.length) {
+        columnToApplyExcessTo = maxWidths.length - numberOfColumns;
+      }
+    }
+  }
+
+  var adjust_widths_for_colspans = function(widths, maxWidths, colspanWidths) {
+    var lastNumberOfColumnsWidth = null;
+    var excess = null;
+
+    each(colspanWidths, function(colspanWidth, index) {
+      lastNumberOfColumnsWidth = get_width_of_last_number_of_columns(maxWidths, index + 1);
+
+      if(colspanWidth && colspanWidth > lastNumberOfColumnsWidth){
+        excess = colspanWidth - lastNumberOfColumnsWidth;
+        spread_out_excess_over_last_number_of_columns(maxWidths, excess, index + 1);
+        set_max_widths_on_non_colspan_columns(widths, maxWidths);
+      }
+    }, this);
+  }
+
+  var adjust_colspans_for_widths = function(widths, maxWidths) {
+    var colspan = null;
+    var lastNumberOfColumnsWidth = null
+
+    each(widths, function(row, rowIndex) {
+      colspan = maxWidths.length - row.length + 1;
+
+      if(colspan > 1) {
+        row[row.length - 1] = get_width_of_last_number_of_columns(maxWidths, colspan);
+      }      
+    }, this);
+  }
+
+  /*
+   * Utility functions
+   */
+  var trim = function(text) {
+    return (text || "").replace( /^\s+|\s+$/g, "" );
+  }
+
+  var each = function(array, callback, context) {
+    var index = 0;
+    var length = array.length;
+
+    while(index < length && callback.call(context, array[index], index) !== false) {
+      index++;
+    }
+  }
+
   var right_pad = function(value, length) {
     var padded = value;
-    
+
     for(var i = 0, j = length - value.length; i < j; i++) {
       padded += " ";
     }
 
     return padded;
   }
-  
-  var get_max_column_widths_of = function(table) {
-    widths = [];
-    
-    $.each(table, function() {
-      var parts = split_row(this);
-      
-      $.each(parts, function(index, value) {
-        if(widths.length <= index) {
-          widths.push(value.length)
-        }
-        else if(value.length > widths[index]) {
-          widths[index] = value.length;
-        }
-      });
-    });
-    
-    return widths;
-  }
-  
-  var format_table = function(table) {
-    var formatted = "";
-    var widths = get_max_column_widths_of(table);
 
-    $.each(table, function() {
-      formatted += "| ";
-      var parts = split_row(this);
-      
-      $.each(parts, function(index, value) {
-        formatted += right_pad(value, widths[index]) + " | ";
-      });
-      
-      formatted += "\n";
-    });
-    
-    return formatted;
-  }
-  
-  var format_content = function(content) {
-    var formatted = "";
-    var current_table = [];
-    var current_number_of_columns = 0;
-    var lines = content.split("\n");
-    
-    $.each(lines, function(){
-      if(is_table_row(this)) {
-        var number_of_columns = split_row(this).length
-        
-        if(current_table.length == 0 || current_number_of_columns == number_of_columns) {
-          current_table.push(this);
-          current_number_of_columns = number_of_columns;
-          return true;
-        }
-      }
-      
-      formatted += format_table(current_table);
-      current_table = [];
-      
-      if(is_table_row(this)) {
-        current_table.push(this);
-        current_number_of_columns = split_row(this).length;
-      }
-      else {
-        formatted += this + "\n";
-      }
-    });
-    
-    formatted += format_table(current_table);
-    return formatted.slice(0, formatted.length - 1);
-  }
-  
   var format_page = function() {
     var textarea = $(TEXTAREA, doc);
     textarea.attr('value', format_content(textarea.attr('value')));
+  }
+  
+  var toggle_wrap = function() {
+    var textarea = $(TEXTAREA, doc);
+    textarea.attr('wrap', this.checked ? 'soft' : 'off');
+
+    if(this.checked) {
+      textarea.removeClass('no_wrap');
+    }
+    else {
+      textarea.addClass('no_wrap');
+    }
   }
   
   var content_load = function(event) {
@@ -118,6 +280,11 @@
       var editButtons = $("div.edit_buttons", doc)
       editButtons.append('<input id="format_button" type="button" accesskey="f" value="Format">');
       $('#format_button', doc).click(format_page);
+      
+      editButtons.after('<div class="edit_options"><input id="toggle_wrap" type="checkbox" accesskey="w" checked="checked" title="Turns on/off wrapping"><span>wrap</span></div>')
+      $('#toggle_wrap', doc).click(toggle_wrap);
+      
+      textarea.attr('wrap', 'soft');
     }
   }
   
